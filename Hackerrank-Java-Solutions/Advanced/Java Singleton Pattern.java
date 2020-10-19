0b8c0d7dec5246534865d8e6c454c66c2d269916
HetDaftary <hetdaftary@gmail.com>
2020-10-19 11:39:23 +0530

    Advanced

---
+++ Hackerrank-Java-Solutions/Advanced/Java Singleton Pattern.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    public String str;
+    private Singleton() {}
+    
+    static Singleton getSingleInstance() {
+        return new Singleton();
+    }
+}
