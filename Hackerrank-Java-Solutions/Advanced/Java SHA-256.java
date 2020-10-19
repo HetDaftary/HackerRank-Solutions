280f136a058795b02f0f644ae463103ec503dd33
HetDaftary <hetdaftary@gmail.com>
2020-10-19 12:51:43 +0530

    Advanced

--- Hackerrank-Java-Solutions/Advanced/Java SHA-256.java
+++ Hackerrank-Java-Solutions/Advanced/Java SHA-256.java
@@ -5,7 +5,7 @@ public class Solution {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         Scanner input = new Scanner(System.in);
-        MessageDigest m = MessageDigest.getInstance("MD5");
+        MessageDigest m = MessageDigest.getInstance("SHA-256");
         m.reset();
         m.update(input.nextLine().getBytes());
         for (byte i : m.digest()) {
